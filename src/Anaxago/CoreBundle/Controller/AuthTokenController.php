e37e4cf55b69f6288500d3f0ee230ee2833c4eab
TKhyarn <spartan1200@hotmail.fr>
2019-05-23 20:33:32 +0200

    fix typo

--- src/Anaxago/CoreBundle/Controller/AuthTokenController.php
+++ src/Anaxago/CoreBundle/Controller/AuthTokenController.php
@@ -17,6 +17,7 @@ use Anaxago\CoreBundle\Form\Type\CredentialsType;
 use Anaxago\CoreBundle\Entity\AuthToken;
 use Anaxago\CoreBundle\Entity\Credentials;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\View\View;
 
 /**
  * Class AuthTokenController
@@ -74,10 +75,10 @@ class AuthTokenController extends Controller
     }
 
     /**
-     * @return \FOS\RestBundle\View\View
+     * @return View
      */
     private function invalidCredentials()
     {
-        return \FOS\RestBundle\View\View::create(['message' => 'Invalid credentials'], Response::HTTP_BAD_REQUEST);
+        return View::create(['message' => 'Invalid credentials'], Response::HTTP_BAD_REQUEST);
     }
 }
